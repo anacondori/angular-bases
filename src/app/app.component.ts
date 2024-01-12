66db883e29503cefafb5c2d5ed302628f8d9f505
anacondori <152404570+anacondori@users.noreply.github.com>
2024-01-12 17:38:20 +0100

    Fin de Seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,35 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,
+            CounterModule, HeroesModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  public title:string = 'Heroes';
+  public counter:number = 10;
+
+/*
+  incrementarPor1(){
+    this.counter=this.counter+1;
+  }
+  decrementarPor1(){
+    this.counter-=1;
+  }*/
+
+  increaseBy(value: number):void{
+       if (value ==0) {
+          this.counter = 10;
+          return ;
+       }
+       this.counter+=value;
+  }
+
 }
